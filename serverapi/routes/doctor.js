e07cdb1139adebfdfeee50dddcdd16042713ce96
Ammar Farooq <101990063+mr-ammar-1@users.noreply.github.com>
2023-11-18 15:41:54 +0500

    Update doctor.js

--- serverapi/routes/doctor.js
+++ serverapi/routes/doctor.js
@@ -2,14 +2,16 @@ import express from 'express';
 const router = express.Router();
 
 
-import {AddDoctor, delDoctor, editDoctor, getDoctor, getDoctors } from "../controllers/doctor";
+import {AddDoctor, delDoctor, editDoctor, getDoctor, getDoctorByUserId, getDoctors, getDoctorsId } from "../controllers/doctor";
 
 router.post("/doctor/add",AddDoctor);
 
 router.get("/doctors",getDoctors);
 router.put("/doctor/:id", editDoctor)
-
+router.get("/doctors/ids",getDoctorsId)
 router.get("/doctor/:id", getDoctor)
+router.get("/doctors/ids",getDoctorsId)
+router.get("/doctorsUser/:id",getDoctorByUserId)
 
 router.delete("/doctors/:id", delDoctor)
 module.exports = router;
