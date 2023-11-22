d1c038aaeb94fad2ff1f06bf624a7c656f9c811f
Ammar Farooq <101990063+mr-ammar-1@users.noreply.github.com>
2023-11-22 10:55:59 +0500

    Update Nurse Controller

--- serverapi/controllers/nurse.js
+++ serverapi/controllers/nurse.js
@@ -36,6 +36,40 @@ export const getNurses = async (req, res) => {
         
     }
 }
+export const getNurseByUserId = async (req,res)=>{
+  try{
+   
+  
+    // Use the Nurse model to find the doctor based on the user._id
+    const nurse = await Nurse.findOne({}).populate('user');
+  
+      if (!nurse) {
+        return res.status(404).json({ message: 'Nurse not found for this user' });
+      }
+  
+      // Access the user._id and compare it with the provided userId
+      if (nurse.user._id.toString() === req.params.id) {
+        res.json(nurse);
+      } else {
+        return res.status(404).json({ message: 'Nurse not found for this user' });
+      }
+  }
+  catch(error){
+  
+  }
+  
+  }
+  
+export const getNursesId = async (req, res) => {
+  // console.log("Hello");
+  try {
+      const nursess = await Nurse.find()
+      const nursesId = nursess.map(u => u._id);
+      res.status(200).json(nursesId);
+  } catch (error) {
+      
+  }
+}
 
 export const getNurse = async (req, res) => {
    
@@ -49,11 +83,11 @@ export const getNurse = async (req, res) => {
 
 export const editNurse = async (req, res) => {
     try {
-        const { nurseId } = req.params;
+        const  nurseId  = req.params.id;
         const updatedData = req.body;
     
         // Update the doctor's information
-        const updatedNurse = await Nurse.findByIdAndUpdate(nurseId, updatedData, { new: true });
+        const updatedNurse = await Nurse.findByIdAndUpdate({_id:nurseId}, updatedData, { new: true });
     
         if (!updatedNurse) {
           return res.status(404).json({ error: 'Nurse not found' });
@@ -61,9 +95,9 @@ export const editNurse = async (req, res) => {
     
         // Update the associated user information
         const userId = updatedNurse.user;
-        const updatedUser = await User.findByIdAndUpdate(userId, updatedData, { new: true });
+        const updatedUser = await User.findByIdAndUpdate({_id:userId}, updatedData, { new: true });
     
-        res.status(200).json({ doctor: updatedDoctor, user: updatedUser });
+        res.status(200).json({ Nurse: updatedNurse, User: updatedUser });
       } catch (error) {
         res.status(500).json({ error: 'An error occurred while updating the Nurse.' });
       }
