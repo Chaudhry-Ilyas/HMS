f4679bafe3ad1086d3f95a0f9ffef002212a7d6c
Ammar Farooq <101990063+mr-ammar-1@users.noreply.github.com>
2023-11-26 12:00:05 +0500

    Update Patients Controller

--- serverapi/controllers/patient.js
+++ serverapi/controllers/patient.js
@@ -1,5 +1,8 @@
+// import {BackendLog} from "../controllers/backend";
 import Patient from"../models/patients"
+import patients_audit from "../models/patients_audit";
 import { hashPassword } from "../utils/auth";
+const BackendController= require("./backend")
 
 export const AddPatient = async (req, res) => {
 
@@ -32,6 +35,18 @@ export const getPatients = async (req, res) => {
         
     }
 }
+export const getPatientsId = async (req, res) => {
+  // console.log("Hello");
+  try {
+      const patients = await Patient.find()
+
+      const patientIds = patients.map(patient => patient._id.toString());
+    res.status(200).json(patientIds);
+
+  } catch (error) {
+      
+  }
+}
 
 export const getPatient = async (req, res) => {
    
@@ -45,20 +60,31 @@ export const getPatient = async (req, res) => {
 
 export const editPatient = async (req, res) => {
     try {
-        const { patientId } = req.params;
+        const  patientId  = req.params.id;
         const updatedData = req.body;
+        const oldPatient = await Patient.findById(patientId);
     
         // Update the doctor's information
-        const updatedPatient = await Patient.findByIdAndUpdate(patientId, updatedData, { new: true });
+        const updatedPatient = await Patient.findByIdAndUpdate({_id:patientId}, updatedData, { new: true });
     
         if (!updatedPatient) {
           return res.status(404).json({ error: 'Patient not found' });
         }
-    
+        const newPatient = await Patient.findById(patientId);
+
+        await patients_audit.create({
+            patientId: patientId,
+            action: 'update',
+            oldValue: oldPatient.toObject(),
+            newValue: newPatient.toObject(),
+        });
+
         // Update the associated user information
        
-        res.status(200).json({ patient: updatedPatient});
+        res.status(200).json({ Patient: updatedPatient});
       } catch (error) {
+        BackendController.AddBackEndLog("Patient.js","EditPatient",error.message)
+
         res.status(500).json({ error: 'An error occurred while updating the Patient.' });
       }
 }
